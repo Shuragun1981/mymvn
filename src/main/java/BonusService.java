2533dc4d6c860e9a8d9be71178bfdad17efb2f2a
Aleksandr Volik <Aleksandr_volik@mail.ru>
2024-02-04 12:09:26 +0300

    Rename percent to Percent

--- src/main/java/BonusService.java
+++ src/main/java/BonusService.java
@@ -1,7 +1,7 @@
 public class BonusService {
     public long calculate(long amount, boolean Registered) {
-        int percent = Registered ? 3 : 1;
-        long bonus = amount * percent / 100;
+        int Percent = Registered ? 3 : 1;
+        long bonus = amount * Percent / 100;
         long limit = 500;
         if (bonus > limit) {
             bonus = limit;
